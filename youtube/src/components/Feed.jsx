03bdff69197f0a4ee8fa86d6a0d3e1df81009c15
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-04-29 21:29:10 +0530

    Feed.jsx done

--- youtube/src/components/Feed.jsx
+++ youtube/src/components/Feed.jsx
@@ -1,7 +1,18 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import { Videos, Sidebar } from "./index";
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", sm: "row" } }}>
       <Box
@@ -11,7 +22,21 @@ const Feed = () => {
           padding: { sx: 0, md: 2 },
         }}
       >
-        Sidebar
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+      </Box>
+      <Box p={2} sx={{ overflow: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>Videos</span>
+        </Typography>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );

23cd60b273351596b6015be9bca1eaced49e7a98
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-05-02 09:14:08 +0530

    SearcbBar.jsx updated

--- youtube/src/components/SearchBar.jsx
+++ youtube/src/components/SearchBar.jsx
@@ -1,28 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-import { Paper, IconButton, Input } from "@mui/material";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={onhandleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
-        paddingLeft: 2,
+        pl: 2,
         boxShadow: "none",
         mr: { sm: 5 },
       }}
     >
-      <Input
+      <input
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
         <SearchIcon />
       </IconButton>
     </Paper>

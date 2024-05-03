826abd7d5d5203151aadedae6cefa8a3aa9d379a
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-05-03 10:16:28 +0530

    all imports done

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+  Navigation,
+} from "./components/index.js";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navigation />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
 export default App;

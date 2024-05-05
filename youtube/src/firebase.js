4491a5f6e51a404c851dd6ab8f4e79f1461113f5
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-05-05 21:34:27 +0530

    firebase configuration done

---
+++ youtube/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC2NPXjOha5ipuT_tmg6CxLd48fEVY-cvQ",
+  authDomain: "newser-422a0.firebaseapp.com",
+  projectId: "newser-422a0",
+  storageBucket: "newser-422a0.appspot.com",
+  messagingSenderId: "503053256476",
+  appId: "1:503053256476:web:8710aef17c65586239dcd9",
+  measurementId: "G-MMJVBPYFCP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;

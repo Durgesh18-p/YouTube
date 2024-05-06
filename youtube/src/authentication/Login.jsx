1e4c2de764dd16b0554c472f94a6279825bde516
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-05-06 17:41:22 +0530

    login updated

--- youtube/src/authentication/Login.jsx
+++ youtube/src/authentication/Login.jsx
@@ -42,67 +42,73 @@ const Login = ({
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <div className={classes.container}>
-        <Avatar className={classes.avatar}>
-          <LockOpenIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
+    <Container
+      style={{
+        marginTop: "5rem",
+      }}
+    >
+      <Container component="main" maxWidth="xs">
+        <div className={classes.container}>
+          <Avatar className={classes.avatar}>
+            <LockOpenIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
 
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-        </form>
-      </div>
-      <Button
-        fullWidth
-        variant="contained"
-        color="primary"
-        style={{ marginTop: "1rem" }}
-        onClick={handleGoogleSignIn}
-        className={classes.submit}
-      >
-        <GoogleIcon style={{ marginRight: "1rem" }} />
-        Sign In with Google
-      </Button>
-      <Typography varient="h1">
-        Already have an account : <Link to="/login">LogIn</Link>
-      </Typography>
+          <form className={classes.form} onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+          </form>
+        </div>
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          style={{ marginTop: "1rem" }}
+          onClick={handleGoogleSignIn}
+          className={classes.submit}
+        >
+          <GoogleIcon style={{ marginRight: "1rem" }} />
+          Sign In with Google
+        </Button>
+        <Typography varient="h1">
+          Already have an account : <Link to="/login">LogIn</Link>
+        </Typography>
+      </Container>
     </Container>
   );
 };

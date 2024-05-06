fdb7f79c06f51819dbf0c563853027a6427aed75
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-05-06 17:50:38 +0530

    app done

--- youtube/src/App.jsx
+++ youtube/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 import {
@@ -7,21 +8,72 @@ import {
   SearchFeed,
   Navigation,
 } from "./components/index.js";
+import Login from "./authentication/Login.jsx";
+// import SignUp from "./authentication/SignUp.jsx";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "./firebase.js";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Box sx={{ backgroundColor: "#000" }}>
-        <Navigation />
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleGoogleSignIn = () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // Handle successful Google sign-in
+        const user = result.user;
+        console.log("Google Sign-In successful. User:", user);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error("Google Sign-In error:", error);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle login logic here
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  if (!email || !password) {
+    return (
+      <BrowserRouter>
         <Routes>
-          <Route path="/" exact element={<Feed />} />
-          <Route path="/video/:id" exact element={<VideoDetail />} />
-          <Route path="/channel/:id" exact element={<ChannelDetail />} />
-          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+          <Route
+            path="/"
+            element={
+              <Login
+                email={email}
+                password={password}
+                setEmail={setEmail}
+                setPassword={setPassword}
+                handleSubmit={handleSubmit}
+                handleGoogleSignIn={handleGoogleSignIn}
+              />
+            }
+          />
         </Routes>
-      </Box>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
+  } else {
+    return (
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: "#000" }}>
+          <Navigation />
+          <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/video/:id" exact element={<VideoDetail />} />
+            <Route path="/channel/:id" exact element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
